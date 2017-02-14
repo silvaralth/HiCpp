08e9d42ebe41dd7e6bf04ebab484209ba1cde74b
silvaralth <silvaralth@gmail.com>
2017-02-14 06:34:47 -0500

    Initial Commit

---
+++ HolaMundo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HolaMundo
+//
+//  Created by Cocoa on 14/02/17.
+//  Copyright © 2017 SergioBallen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

afb4edc2f06cc34d300f214ea02b0325e397e6ef
silvaralth <silvaralth@gmail.com>
2017-02-21 04:22:59 -0500

    digit validation

--- HolaMundo/main.cpp
+++ HolaMundo/main.cpp
@@ -3,44 +3,47 @@
 //  HolaMundo
 //
 //  Created by Cocoa on 14/02/17.
-//  Copyright © 2017 SergioBallen. All rights reserved.
+//  Copyright © 2017 SergioBallen COD: 1629770 - 2711
 //
 
 #include <iostream>
-#include <sstream>
+#include <string>
+#include <cctype>
+
 using namespace std;
 
-void validame (stringstream var){
-    
-    //stringstream ss;
-    //ss.str("var");
-    
-    string s;
-    int i;
-    double d;
-    
-    // Extrae, convierte a los tipos apropiados y asigna, todo en una sola línea:
-    var >> s >> i >> d;
-}
+bool validame (string);
 
 int main(int argc, const char * argv[]) {
-    //
+
     cout << "Hola hola ;) \n";
+    string valor;
     
-    stringstream valor;
-    cout << "Dame un numero, que yo soy todo un adivino ... ";
-    //cin >> valor;
-    
-    //validame(valor);
-    
-    /*
-    stringstream sstm;
-    sstm << "El numero que me diste fue: " << valor << "  :P";
-    string result = sstm.str();
-    */
-    //cout << "El numero que me diste fue: " << valor << "  :P \n";
-    
-    
-    
+    do {
+        cout << "Dame un numero, que yo soy todo un adivino ... ";
+        getline(cin, valor);
+        
+        if (validame(valor) == true){
+            cout << "el numero es " << valor << endl;
+        } else {
+            cout << "\nno ingresaste un numero... ¬¬" << endl;
+        }
+    } while (validame(valor) == false);
+
     return 0;
+}
+
+bool validame (string var){
+    bool resultado = false;
+    double size = var.length();
+
+    for (int i = 0; i < size ; i++) {
+        if ( isdigit(var[i]) && size >= 1 ){
+            resultado = true;
+        } else {
+            resultado = false;
+            break;
+        }
+    }
+    return resultado;
 }
